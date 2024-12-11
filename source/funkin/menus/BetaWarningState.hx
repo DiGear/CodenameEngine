c316fcaf5f79d982825bf53659974227e665df1e
Idunno1 <thegamemaster210@gmail.com>
2024-12-10 22:35:35 -0600

    assets

--- source/funkin/menus/BetaWarningState.hx
+++ source/funkin/menus/BetaWarningState.hx
@@ -39,7 +39,7 @@ class BetaWarningState extends MusicBeatState {
 		new Warning("WARNING", "*Sam*",
 					["*" => 0x680000]),
 		new Warning("COCK", ":)"),
-		new Warning("Attention", "If you or a loved one has been diagnosed with *Mesothelioma* you may to be entitled to financial compensation. *Mesothelioma* is a rare cancer linked to asbestos exposure. Exposure to asbestos in the Navy, shipyards, mills, heating, construction or the automotive industries may put you at risk. Please don't wait, call 1-800-99 LAW USA today for a free legal consultation and financial information packet. *Mesothelioma* patients call now! 1-800-99 LAW USA ",
+		new Warning("Attention", "If you or a loved one has been diagnosed with *Mesothelioma* you may to be entitled to financial compensation. *Mesothelioma* is a rare cancer linked to asbestos exposure. Exposure to asbestos in the Navy, shipyards, mills, heating, construction or the automotive industries may put you at risk. Please don't wait, call 1-800-99 EAT ASS today for a free legal consultation and financial information packet. *Mesothelioma* patients call now! 1-800-99 EAT ASS ",
 					["*" => 0x519A61]),
 		new Warning("hey guys", "This build <may< have *danny* in it",
 					["*" => 0xA68FF0, "<" => 0xFFFFF783]),
